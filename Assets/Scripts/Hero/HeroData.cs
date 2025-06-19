e8ba12a87984bd06730d15127eb08c7b95e17a78
endsun1234 <endsun1234@naver.com>
2025-06-19 18:00:37 +0900

    인트로 작업중
    캐릭터 선택 UI 작업중...

--- Assets/Scripts/Hero/HeroData.cs
+++ Assets/Scripts/Hero/HeroData.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// 주인공 기본 능력치를 포함하는 설정 데이터 클래스
@@ -12,6 +14,14 @@ public class HeroData : ScriptableObject
     [SerializeField] float _speed;      // 기본 이동 속력
     [SerializeField] float _baseExp;    // 기본 경험치
     [SerializeField] float _expIncrementRate; // 경험치 배수
+
+    [Header("----- UI용 -----")]
+    [SerializeField] Sprite _heroIcon;               // 영웅 아이콘
+    [SerializeField] string _nameText;          // 영웅 이름 텍스트
+    [SerializeField] string _descText;          // 영웅 능력치 텍스트
+    [SerializeField] string _skillNameText;     // 영웅 스킬이름 텍스트
+    [SerializeField] Sprite _skillIcon;          // 영웅 스킬 아이콘
+    [TextArea(3, 5)] [SerializeField] string _skillDescription;  // 영웅 스킬 설명
 
     public float MaxHp => _maxHp;
     public float Speed => _speed;
